19282d1d7848b670a31268a1e99c1a6cd1a650e0
sayagaffy <rgaffyagb@gmail.com>
2026-02-03 15:56:00 +0700

    chore: prepare for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true,
     formats: ["image/webp", "image/avif"],
   },
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
-  assetPrefix: process.env.NODE_ENV === "production" ? "" : undefined,
 };
 
 module.exports = nextConfig;
